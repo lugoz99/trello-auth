267a88adc75f8067bb74d83d41900a0ae8361b4e
Luis Fernando Gonzalez <88568999+lugoz99@users.noreply.github.com>
2025-04-13 06:33:40 +0000

    guard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { TokenService } from './token.service';
+import { tap } from 'rxjs/operators';
+import { ResponseLogin } from '@models/auth-model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +12,18 @@ export class AuthService {
 
 
   private API_URL = environment.API_URL;
-  constructor( private httpCliente:HttpClient ) { }
+  constructor( private httpCliente:HttpClient, private tokenService:TokenService) { }
 
   login(email: string, password: string) {
     // Simulate a login check
-    return this.httpCliente.post(`${this.API_URL}/api/v1/auth/login`,{
+    return this.httpCliente.post<ResponseLogin>(`${this.API_URL}/api/v1/auth/login`,{
       email,
       password
-    })
+    }).pipe(
+      tap( resp => {
+          this.tokenService.saveToken( resp.access_token )
+      } )
+    )
   }
 
   regiter(email: string, password: string,name:string){
